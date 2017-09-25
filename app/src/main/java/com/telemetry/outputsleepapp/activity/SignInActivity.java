fca605128025ed5616618505613d02721ad16393
Sergey Penkal <sergey.penkal@gmail.com>
2017-09-25 16:18:24 +0300

    Fixed all remark

--- app/src/main/java/com/telemetry/outputsleepapp/activity/SignInActivity.java
+++ app/src/main/java/com/telemetry/outputsleepapp/activity/SignInActivity.java
@@ -2,6 +2,8 @@ package com.telemetry.outputsleepapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
 import com.telemetry.outputsleepapp.R;
 
 public class SignInActivity extends AppCompatActivity{
@@ -10,5 +12,17 @@ public class SignInActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_in);
+    }
+
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.sign_in_label:
+
+                break;
+
+            case R.id.sign_in_forgot_password:
+
+                break;
+        }
     }
 }
--- app/src/main/java/com/telemetry/outputsleepapp/activity/SignUpActivity.java
+++ app/src/main/java/com/telemetry/outputsleepapp/activity/SignUpActivity.java
@@ -2,13 +2,48 @@ package com.telemetry.outputsleepapp.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 import com.telemetry.outputsleepapp.R;
 
 public class SignUpActivity extends AppCompatActivity{
 
+    ImageView signUpCheckbox;
+    boolean signUpCheckboxFlag = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.sign_up);
+
+        signUpCheckbox = (ImageView)findViewById(R.id.sign_up_checkbox);
     }
+
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case  R.id.sign_up_label:
+
+                break;
+
+            case R.id.sign_up_checkbox:
+                if(signUpCheckboxFlag)
+                    signUpCheckbox.setImageResource(R.drawable.checkbox_on);
+                else
+                    signUpCheckbox.setImageResource(R.drawable.checkbox_off);
+                signUpCheckboxFlag = !signUpCheckboxFlag;
+                break;
+
+            case R.id.sign_up_the_terms:
+
+                break;
+
+            case R.id.sign_up_facebook:
+
+                break;
+
+        }
+    }
+
 }
